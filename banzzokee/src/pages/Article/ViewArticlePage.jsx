3dac493b4d818304ae8a36086a111a4c5afe3cf1
SoMin-Yoo <somin3994@naver.com>
2024-02-08 21:39:16 +0900

    태그 수정

--- banzzokee/src/pages/Article/ViewArticlePage.jsx
+++ banzzokee/src/pages/Article/ViewArticlePage.jsx
@@ -2,13 +2,12 @@ import styles from './ViewArticlePage.module.css';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import BackHeader from '../../components/common/header/BackHeader';
 import Nav from '../../components/common/nav/Nav';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 export default function ViewArticlePage() {
-  // const accessToken = JSON.parse(sessionStorage.getItem('accessToken'));
-  // setCookie('accessToken', data['accessToken'], { path: '/' });
+  const { id } = useParams();
   const [adoption, setAdoption] = useState({});
-<<<<<<< HEAD
   const navigate = useNavigate();
 
   const openEdit = () => {
@@ -38,64 +37,32 @@ export default function ViewArticlePage() {
     }
   }
 
-=======
-  // sessionStorage.setItem('accessToken', JSON.stringify(data.accessToken));
-  const { adoptionId } = useParams();
->>>>>>> 6865fdbb65154337fd8a982f3c81465145d1fe0e
   useEffect(() => {
-    const getArticle = async () => {
-      try {
-        const response = await axios.get(`http://localhost:3001/adoption/1`);
-        setAdoption(response.data);
-        console.log(adoption);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getArticle();
-  }, [adoptionId]);
-  // const adoption = JSON.parse(sessionStorage.getItem('articleInfo'));
-  const tags = [adoption.breed, adoption.size, adoption.neutering ? '중성화' : '중성화안됨', adoption.gender, `${adoption.age}살`, adoption.healthChecked ? '건강검진' : '검진x', `보호소 등록: ${adoption.registeredAt}`];
+    getAdoption();
+  }, [id]);
 
   return (
     <>
-      <div>
-        <BackHeader></BackHeader>
-        <div className={styles.container}>
-          <div className={styles.article}>
-            <div className={styles.articleHeader}>
-              <Link to="/OtherMyPage">
-                <div className={styles.headerLeft}>
-                  <div className={styles.userImage}>
-                    <img src="../../../public/User.png" alt="" style={{ width: '14px', height: '14px' }} />
-                  </div>
-                  <div className={styles.nameAndDate}>
-                    <div className={styles.name}>{adoption.nickname}</div>
-                    <div className={styles.date}>{adoption.createdAt}</div>
-                  </div>
+      <BackHeader></BackHeader>
+      <div className={styles.container}>
+        <div className={styles.article}>
+          <div className={styles.articleHeader}>
+            <Link to="/OtherMyPage">
+              <div className={styles.headerLeft}>
+                <div className={styles.userImage}>
+                  <img src="../../../public/User.png" alt="" style={{ width: '14px', height: '14px' }} />
                 </div>
-              </Link>
-              <Link className="chat" to="/Message">
-                <div className={styles.messageButton}>
-                  메세지
-                  <img className={styles.messageIcon} src="../../../public/Message.png" />
+                <div className={styles.nameAndDate}>
+                  <div className={styles.name}>user_123</div>
+                  <div className={styles.date}>2023-01-03</div>
                 </div>
-              </Link>
-            </div>
-
-            <div className={styles.articlePhotos}>
-              <div className={styles.imgContainer}>
-                <img src={adoption.imageUrls} alt="" />
               </div>
-
-              <div className={styles.status}>분양중</div>
-            </div>
-            <div className={styles.articleTexts}>
-              <div className={styles.titleAndSave}>
-                <div className={styles.title}>{adoption.title}</div>
-                <img src="../../../public/save.svg" alt="저장하기" style={{ width: '45px', height: '30px' }} />
+            </Link>
+            <Link className="chat" to="/Message">
+              <div className={styles.messageButton}>
+                메세지
+                <img className={styles.messageIcon} src="../../../public/Message.png" />
               </div>
-<<<<<<< HEAD
             </Link>
             <button style={{padding:0}} onClick={openEdit}>
               <img src='../../../public/edit.svg' style={{ transform: 'rotate(90deg)' }} />
@@ -111,19 +78,11 @@ export default function ViewArticlePage() {
               </button>
             </div>
           </div>
-=======
->>>>>>> 6865fdbb65154337fd8a982f3c81465145d1fe0e
 
-              <div className={styles.tags}>
-                {tags.map((tag, index) => (
-                  <div key={index} className={styles.tag}>
-                    {tag}
-                  </div>
-                ))}
-              </div>
-              <div className={styles.body}>{adoption.content}</div>
+          <div className={styles.articlePhotos}>
+            <div className={styles.imgContainer}>
+              <img src="../../../public/dog.webp" alt="" />
             </div>
-<<<<<<< HEAD
 
             <div className={styles.status}>{adoption.status}</div>
           </div>  
@@ -150,12 +109,10 @@ export default function ViewArticlePage() {
               )}
             </div>
             <div className={styles.body}>{adoption.content}</div>
-=======
->>>>>>> 6865fdbb65154337fd8a982f3c81465145d1fe0e
           </div>
         </div>
-        <Nav></Nav>
       </div>
+      <Nav></Nav>
     </>
   );
 }
