f07edbc710303636bfde1df28c3e9fe46c98a852
SoMin-Yoo <somin3994@naver.com>
2024-02-14 21:54:28 +0900

    Filter, MainPage 연동

--- banzzokee/src/components/ArticleList.jsx
+++ banzzokee/src/components/ArticleList.jsx
@@ -26,8 +26,27 @@ export default function ArticleList() {
         {articleList &&
           articleList.map((adoption) => (
             <li key={adoption.id}>
-              <Link to={`/ArticleList/${adoption.id}`} key={adoption.id}>
-                {adoption.title}
+              <Link to={`/ArticleList/${adoption.id}`} key={adoption.id} className={styles.link}>
+                <div className={styles.image}>
+                  <img src="../../../public/dog.webp" alt="" />
+                  <div className={styles.status}>{adoption.status}</div>
+                </div>
+                <div>
+                  <div className={styles.user}>
+                    <div className={styles.userInfo}>
+                      <img src="../../../public/User.png" alt="" style={{ width: '14px', height: '14px' }} />
+                      <div className={styles.name}>user_123</div>
+                    </div>
+                      <div className={styles.date}>2023-01-03</div>
+                  </div>
+                  <div className={styles.title}>{adoption.title}</div>
+                  <div className={styles.tags}>
+                    <div className={styles.tag}>{adoption.tags.breeds}</div>
+                    <div className={styles.tag}>{adoption.tags.size}</div>
+                    <div className={styles.tag}>{adoption.tags.healthChecked}</div>
+                    <div className={styles.tag}>{adoption.tags.gender}</div>
+                  </div>
+                </div>
               </Link>
             </li>
           ))}
