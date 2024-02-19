e2edcabe76e82c73307e811c8b20b5130d93c8de
SoMin-Yoo <somin3994@naver.com>
2024-02-19 11:51:05 +0900

    GoogleRegister 페이지 api 연동& CSS 수정

--- banzzokee/src/pages/register/GoogleRegister.jsx
+++ banzzokee/src/pages/register/GoogleRegister.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
-import Back from "../../components/common/back/Back";
 import styles from './GoogleRegister.module.css'
 import BackHeader from "../../components/common/header/BackHeader";
+import { useCookies } from "react-cookie";
+import axios from "axios";
 
 export default function GoogleRegister() {
+
+  const [cookies, setCookie, removeCookie] = useCookies();
   const [nickname, setNickname] = useState("");
   const onChange = (e) => {
     const { target: { name, value } } = e;
@@ -12,22 +15,42 @@ export default function GoogleRegister() {
     }  
   }
 
+  const doGoogleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const inputdata = { nickname: nickname };
+      const config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://server.banzzokee.homes/api/oauth2/sign-up',
+        headers: {},
+        data: inputdata,
+      };
+      const response = await axios.request(config);
+      setCookie('accessToken', response.data['accessToken'], { path: '/', secure: true });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <BackHeader/>
       <div className={styles.google_page}>
-        <div className={styles.back}>
-        </div>
         <div className={styles.GoogleRegister}>
           <h2>회원가입</h2>
-          <form className={styles.container}>
+          <form className={styles.container} onSubmit={doGoogleSignUp}>
             <label>닉네임</label>
-            <input type="text" name="nickname" value={nickname} onChange={onChange}  />
-            <button type="submit" id="confirmButton" className={styles.confirmButton}>중복확인</button>
+            <input type="text" name="nickname" value={nickname} onChange={onChange} maxLength='10' />
+            <button type="submit" id="registerButton" className={styles.registerButton}>가입하기</button>
           </form>
-          <button type="submit" id="registerButton" className={styles.registerButton}>가입하기</button>
         </div>
       </div>
     </>
-  )
-}  
+  );
+}
+
+
+
+
+
