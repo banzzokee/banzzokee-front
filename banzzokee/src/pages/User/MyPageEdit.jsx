350b4665e6468692be21edefdf37e2ef9a515e50
Heejun Yang <daniel971219@gmail.com>
2024-02-19 22:10:26 +0900

    myPage edit api연동 완료

--- banzzokee/src/pages/User/MyPageEdit.jsx
+++ banzzokee/src/pages/User/MyPageEdit.jsx
@@ -2,15 +2,17 @@ import styles from './MyPageEdit.module.css';
 import BackHeader from '../../components/common/header/BackHeader';
 import { useState } from 'react';
 import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams, useLocation } from 'react-router-dom';
 export default function MyPageEdit() {
-  const navigate = useNavigate();
+  const { state } = useLocation();
   // const userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
   const accessToken = JSON.parse(sessionStorage.getItem('accessToken'));
-
+  const navigate = useNavigate();
   // const [newNickname, setNewNickname] = useState(nickname);
   // const [newIntroduce, setNewIntroduce] = useState(introduce);
-  const [newInfo, setNewInfo] = useState();
+  const [newInfo, setNewInfo] = useState(state);
+  const [profileImage, setProfileImage] = useState(state.profileImgUrl);
+  const [submitImage, setSubmitImage] = useState(null);
 
   const onChange = (e) => {
     const { name, value } = e.target;
@@ -19,9 +21,7 @@ export default function MyPageEdit() {
       [name]: value,
     });
   };
-
-  const [profileImage, setProfileImage] = useState(null);
-  const [submitImage, setSubmitImage] = useState(null);
+  console.log(state);
 
   const handleImageChange = (e) => {
     const selectedImage = e.target.files[0];
@@ -42,22 +42,26 @@ export default function MyPageEdit() {
     //   alert('수정완료');
     //   navigate('/MyPage');
     // });
-
     let sendData = new FormData();
-    sendData.append('profileUrl', submitImage);
-    sendData.append('nickname', newInfo.nickname);
-    sendData.append('introduce', newInfo.introduce);
+    sendData.append('profileImg', submitImage);
+    sendData.append('request', new Blob([JSON.stringify(newInfo)], { type: 'application/json' }));
+    // console.log('formdata', sendData);
+    // sendData.append('profileUrl', submitImage);
+    // sendData.append('request', newInfo);
+    // sendData.append('introduce', JSON.stringify(newInfo.introduce));
     try {
       const config = {
         method: 'patch',
         url: 'https://server.banzzokee.homes/api/users/me',
-        headers: { 'Content-Type': 'multipart/form-data', Authorization: `Bearer ${accessToken}` },
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${accessToken}`,
+        },
         data: sendData,
       };
       await axios.request(config).then((response) => {
         alert('수정완료');
         navigate('/MyPage');
-        console.log(response);
         sessionStorage.setItem('userInfo', JSON.stringify(newInfo));
       });
     } catch (error) {
@@ -83,10 +87,10 @@ export default function MyPageEdit() {
             </div>
             <div className={styles.shelterInfo}>
               <p>닉네임:</p>
-              <input className={styles.input} type="text" name="nickname" onChange={onChange} />
+              <input className={styles.input} type="text" name="nickname" value={newInfo.nickname} onChange={onChange} />
               {/* value={newInfo.nickname} 추가해야됨*/}
               <p>자기 소개:</p>
-              <textarea className={styles.textarea} type="text" name="introduce" onChange={onChange} />
+              <textarea className={styles.textarea} type="text" name="introduce" value={newInfo.introduce} onChange={onChange} />
               {/* value={newInfo.introduce} */}
             </div>
           </div>
