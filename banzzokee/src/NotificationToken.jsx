d1ec68d1bcae2c877f8b85421f2709783d91e990
SoMin-Yoo <somin3994@naver.com>
2024-02-21 17:45:37 +0900

    알림 수정

--- banzzokee/src/NotificationToken.jsx
+++ banzzokee/src/NotificationToken.jsx
@@ -1,63 +1,132 @@
-// import { getMessaging } from "firebase/messaging";
-// import { initializeApp } from "firebase/app";
-// export default function NotificationToken()  {
-//   useEffect(() => {
-//     getMessaging
-//       .getToken()
-//       .then((currentToken) => {
-//         if (currentToken) {
-//           sendTokenToServer(currentToken);
-//           updateUIForPushEnabled(currentToken);
-//         } else {
-//           console.log('No Instance ID token available.');
-//         }
-//       })
-//       .catch((err) => {
-//         console.log('Error ', err);
-//       });
+import { getMessaging, getToken, onTokenRefresh } from "firebase/compat/messaging";
+import { initializeApp } from "firebase/compat/app";
+import { useEffect } from "react";
+import axios from "axios";
+// import React from 'react';
+// import firebase from 'firebase';
 
-//     getMessaging.onTokenRefresh(() => {
-//       getMessaging
-//         .getToken()
-//         .then((refreshedToken) => {
-//           console.log('Token refreshed. ', refreshedToken);
-//           sendTokenToServer(refreshedToken);
-//         })
-//         .catch((err) => {
-//           console.log('Unable to retrieve refreshed token. ', err);
-//         });
-//     });
 
-//     getMessaging.onMessage((payload) => {
-//       console.log('Message received. ', payload);
-//     });
-//   }, []);
+const firebaseConfig = {
+  apiKey: "AIzaSyBnucp4FYPJDz9Dg8qr5abHbTdMGC4Nem4",
+  authDomain: "banzzokee-6e38c.firebaseapp.com",
+  projectId: "banzzokee-6e38c",
+  storageBucket: "banzzokee-6e38c.appspot.com",
+  messagingSenderId: "706417801572",
+  appId: "1:706417801572:web:3ce06d0ae06cde78a1a271",
+  measurementId: "G-YPCP2D21P6"
+};
 
-//   const sendTokenToServer = (token) => {
-//     const accessToken = sessionStorage.getItem('accessToken');
+const firebaseApp = initializeApp(firebaseConfig);
 
-//     axios({
-//       method: 'put',
-//       url: '/api/notifications/tokens',
-//       headers: {
-//         'Authorization': `Bearer ${accessToken}`,
-//         'Content-Type': 'application/json',
-//       },
-//       data: {
-//         token: token,
-//       },
-//     })
-//     .then((response) => {
-//       console.log('Token sent to server successfully:', response);
-//     })
-//     .catch((error) => {
-//       console.error('Error sending token to server:', error);
-//     });
-//   };
+export default function NotificationToken()  {
 
-//   return(
-//     <></>
-//   )
+  useEffect(() => {
+    const messaging = getMessaging();
 
-// }
+    getToken(messaging).then((currentToken) => {
+      if (currentToken) {
+        sendTokenToServer(currentToken);
+        // updateUIForPushEnabled(currentToken);
+      } else {
+        console.log('No registration token available. Request permission to generate one.');
+      }
+    }).catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+    });
+
+    onTokenRefresh(messaging, () => {
+      getToken(messaging).then((refreshedToken) => {
+        console.log('Token refreshed. ', refreshedToken);
+        sendTokenToServer(refreshedToken);
+      }).catch((err) => {
+        console.log('Unable to retrieve refreshed token. ', err);
+      });
+    });
+
+    messaging.onMessage((payload) => {
+      console.log('Message received. ', payload);
+    });
+  }, []);
+
+  const sendTokenToServer = (token) => {
+    const accessToken = sessionStorage.getItem('accessToken');
+
+    axios({
+      method: 'put',
+      url: 'https://server.banzzokee.homes/api/notifications/tokens',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+      data: {
+        token: token,
+      },
+    })
+    .then((response) => {
+      console.log('Token sent to server successfully:', response);
+    })
+    .catch((error) => {
+      console.error('Error sending token to server:', error);
+    });
+  };
+
+
+
+
+
+  // useEffect(() => {
+  //   const messaging = getMessaging();
+
+  //     getToken(messaging)
+  //     .then((currentToken) => {
+  //       if (currentToken) {
+  //         sendTokenToServer(currentToken);
+  //         // updateUIForPushEnabled(currentToken);
+  //       } else {
+  //         console.log('No Instance ID token available.');
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       console.log('Error ', err);
+  //     });
+
+  //   messaging.onTokenRefresh(() => {
+  //       getToken(messaging)
+  //       .then((refreshedToken) => {
+  //         console.log('Token refreshed. ', refreshedToken);
+  //         sendTokenToServer(refreshedToken);
+  //       })
+  //       .catch((err) => {
+  //         console.log('Unable to retrieve refreshed token. ', err);
+  //       });
+  //   });
+
+  //   messaging.onMessage((payload) => {
+  //     console.log('Message received. ', payload);
+  //   });
+  // }, []);
+
+  // const sendTokenToServer = (token) => {
+  //   const accessToken = sessionStorage.getItem('accessToken');
+
+  //   axios({
+  //     method: 'put',
+  //     url: 'https://server.banzzokee.homes/api/api/notifications/tokens',
+  //     headers: {
+  //       'Authorization': `Bearer ${accessToken}`,
+  //       'Content-Type': 'application/json',
+  //     },
+  //     data: {
+  //       token: token,
+  //     },
+  //   })
+  //   .then((response) => {
+  //     console.log('Token sent to server successfully:', response);
+  //   })
+  //   .catch((error) => {
+  //     console.error('Error sending token to server:', error);
+  //   });
+  // };
+
+}
 
--- banzzokee/src/components/Firebase.js
+++ banzzokee/src/components/Firebase.js
@@ -1,25 +1,25 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
-// importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
-// import 'firebase/messaging';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
+// // importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
+// // import 'firebase/messaging';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBnucp4FYPJDz9Dg8qr5abHbTdMGC4Nem4",
-  authDomain: "banzzokee-6e38c.firebaseapp.com",
-  projectId: "banzzokee-6e38c",
-  storageBucket: "banzzokee-6e38c.appspot.com",
-  messagingSenderId: "706417801572",
-  appId: "1:706417801572:web:3ce06d0ae06cde78a1a271",
-  measurementId: "G-YPCP2D21P6"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBnucp4FYPJDz9Dg8qr5abHbTdMGC4Nem4",
+//   authDomain: "banzzokee-6e38c.firebaseapp.com",
+//   projectId: "banzzokee-6e38c",
+//   storageBucket: "banzzokee-6e38c.appspot.com",
+//   messagingSenderId: "706417801572",
+//   appId: "1:706417801572:web:3ce06d0ae06cde78a1a271",
+//   measurementId: "G-YPCP2D21P6"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-// const messaging = firebase.messaging();
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// // const messaging = firebase.messaging();
