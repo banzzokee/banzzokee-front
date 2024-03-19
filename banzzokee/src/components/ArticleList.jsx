b076e339f3b7ea4f0a5d7da3fb11d81dad4ea4bc
SoMin-Yoo <somin3994@gmail.com>
2024-03-19 23:55:15 +0900

    🎨 React-Query 적용

--- banzzokee/src/components/ArticleList.jsx
+++ banzzokee/src/components/ArticleList.jsx
@@ -1,108 +1,97 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { useInfiniteQuery } from 'react-query';
 import styles from './ArticleList.module.css';
 import Tags from './Tags';
 
-import { useInView } from 'react-intersection-observer';
-
 export default function ArticleList({ sortBy, appliedFilters }) {
-  const [articleList, setArticleList] = useState([]);
-  const [hasMore, setHasMore] = useState(true);
-  const [page, setPage] = useState(0);
-
-  console.log('page:', page);
-  const { ref, inView } = useInView();
-
-  const getArticleList = async () => {
-    try {
-      if (hasMore) {
-        console.log('Getarticle::: page:', page);
-        console.log('Getarticle::: inview:', inView);
-        console.log('Fetching data...');
-        const config = {
-          method: 'get',
-          // url: `https://server.banzzokee.homes/api/adoptions?page=${page}&size=7&direction=${sortBy}`,
-          url: `https://server.banzzokee.homes/api/adoptions`,
-          params: {
-            breed: appliedFilters.breed.join(','),
-            dogSize: appliedFilters.size,
-            neutering: appliedFilters.neutering,
-            healthChecked: appliedFilters.healthChecked,
-            gender: appliedFilters.gender,
-            minAge: appliedFilters.minAge,
-            maxAge: appliedFilters.maxAge,
-            direction: `${sortBy}`,
-            page: page,
-            size: 7,
-          },
-        };
-        console.log('config:', config);
-        console.log('url:', config.url);
-        console.log('params:', config.params);
-        const response = await axios.request(config);
-        console.log('Response:', response);
-        const filteredList = response.data.content;
-        console.log('FilteredList:', filteredList);
-        setArticleList([...articleList, ...filteredList]);
-        if (response.data.content.length < 7) {
-          setHasMore(false);
-          console.log('hasmore:', hasMore);
-        }
-      }
-      if (hasMore) {
-        setPage((page) => page + 1);
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
+  const fetchArticles = async ({ pageParam = 0 }) => {
+    const response = await axios.get('https://server.banzzokee.homes/api/adoptions', {
+      params: {
+        breed: appliedFilters.breed.join(','),
+        dogSize: appliedFilters.size,
+        neutering: appliedFilters.neutering,
+        healthChecked: appliedFilters.healthChecked,
+        gender: appliedFilters.gender,
+        minAge: appliedFilters.minAge,
+        maxAge: appliedFilters.maxAge,
+        direction: `${sortBy}`,
+        page: pageParam,
+        size: 7,
+      },
+    });
+    return response.data.content;
   };
 
-
-  useEffect(() => {
-    console.log('sortby, filter applied:::::::::::::');
-    setPage(0);
-    console.log('setPage0 :page', page);
-    setArticleList([]);
-
-    getArticleList();
-    console.log('after getlist :page', page);
-  }, [sortBy, appliedFilters]);
-
-  useEffect(() => {
-    if (inView && page !== 0 && hasMore) {
-      console.log('inview getArticleList: page', page);
-      getArticleList();
+  const { data, fetchNextPage, hasNextPage, isFetching, isLoading, isError } = useInfiniteQuery(
+    ['articleList', sortBy, appliedFilters],
+    fetchArticles,
+    {
+      getNextPageParam: (lastPage, pages) => {
+        return lastPage.length === 7 ? pages.length : undefined;
+      },
     }
-  }, [inView]);
+  );
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {isError.message}</div>;
+
   return (
     <div className={styles.listBox}>
       <ul>
-        {articleList &&
-          articleList.map((adoption) => (
-            <li key={adoption.id}>
-              <Link to={`/ArticleList/${adoption.adoptionId}`} key={adoption.adoptionId} className={styles.link}>
-                <div className={styles.imageContainer}>
-                  {adoption.imageUrls ? <img src={adoption.imageUrls[0]} alt={adoption.title} /> : <img src="../../../public/dog.webp" alt="" />}
-                  {adoption.status ? <div className={adoption.status.value == '분양중' ? styles.statusAdopting : adoption.status.value == '예약중' ? styles.statusReserving : styles.statusFinished}>{adoption.status.value}</div> : <div className={styles.status}>loading</div>}
-                </div>
-                <div className={styles.infoBox}>
-                  <div className={styles.user}>
-                    <div className={styles.name}>{adoption.userNickname.substring(0, 15)}</div>
-                    <div className={styles.date}>{adoption.createdAt.substring(0, 10)}</div>
+        {data.pages.map((page, pageIndex) => (
+          <React.Fragment key={pageIndex}>
+            {page.map((adoption) => (
+              <li key={adoption.id}>
+                <Link to={`/ArticleList/${adoption.adoptionId}`} className={styles.link}>
+                  <div className={styles.imageContainer}>
+                    {adoption.imageUrls ? (
+                      <img src={adoption.imageUrls[0]} alt={adoption.title} />
+                    ) : (
+                      <img src="../../../public/dog.webp" alt="" />
+                    )}
+                    {adoption.status ? (
+                      <div
+                        className={
+                          adoption.status.value === '분양중'
+                            ? styles.statusAdopting
+                            : adoption.status.value === '예약중'
+                            ? styles.statusReserving
+                            : styles.statusFinished
+                        }
+                      >
+                        {adoption.status.value}
+                      </div>
+                    ) : (
+                      <div className={styles.status}>loading</div>
+                    )}
                   </div>
-                  <div className={styles.title}>{adoption.title}</div>
-                  <div className={styles.tags}>
-                    <Tags adoption={adoption}></Tags>
+                  <div className={styles.infoBox}>
+                    <div className={styles.user}>
+                      <div className={styles.name}>{adoption.userNickname.substring(0, 15)}</div>
+                      <div className={styles.date}>{adoption.createdAt.substring(0, 10)}</div>
+                    </div>
+                    <div className={styles.title}>{adoption.title}</div>
+                    <div className={styles.tags}>
+                      <Tags adoption={adoption}></Tags>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </li>
-          ))}
-        <div className={styles.observer} ref={ref}>
-          {hasMore ? 'loading...' : 'end'}
-        </div>
+                </Link>
+              </li>
+            ))}
+          </React.Fragment>
+        ))}
       </ul>
+      {isFetching ? (
+        <div className={styles.observer}>Loading more...</div>
+      ) : (
+        hasNextPage && (
+          <button className={styles.loadMoreButton} onClick={() => fetchNextPage()} disabled={isFetching}>
+            {isFetching ? 'Loading...' : 'Load More'}
+          </button>
+        )
+      )}
     </div>
   );
 }
